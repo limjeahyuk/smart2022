f24bccc2c363a7e2bdc59f5859165926a412c6b4
혁쨩 <lim0202jh@naver.com>
2022-03-10 15:15:01 +0900

    한글수정

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          아아아아아아아ㅏ아아앙 <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
