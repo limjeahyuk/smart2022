a05b6b4d066b930555c83486e1a98d2a0ce08c29
혁쨩 <lim0202jh@naver.com>
2022-04-07 16:53:34 +0900

    11

--- src/components/WeatherCard.js
+++ src/components/WeatherCard.js
@@ -1,5 +1,4 @@
 import { Typography } from '@mui/material';
-import Box from '@mui/material/Box';
 import { weather_mapping_data } from '../dataset/WeatherData';
 import Grid from '@mui/material/Grid';
 
